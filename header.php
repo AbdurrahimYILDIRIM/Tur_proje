de3514b52a060bd540696a07216951722ab3eacf
AbdurrahimYILDIRIM <abdryldrm2018@gmail.com>
2020-03-18 21:55:25 +0300

    navbar geçişli renk kodu css e eklendi. nav-brand yazı rengi değiştirildi.

--- header.php
+++ header.php
@@ -15,44 +15,44 @@
   <!-- Bu şablona özel stil-->
   <link href="css/modern-business.css" rel="stylesheet">
   <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <link rel="stylesheet"
+  href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 
+ <style> .navbar-brand{ color:#8b17cf } </style>
 </head>
 
 <body>
-<!--navbar  Konum ve içeriği-->
-<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-primary fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">GeziGen.TuR <i class="fas fa-umbrella-beach"></i></a>
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="about.php">About</a>
-          <div class="dropdown-divider"></div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="services.php">Services</a>
-          <div class="dropdown-divider"></div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contact.php">Contact</a>
-        </li>
+  <!--navbar  Konum ve içeriği   navbar-expand-lg navbar-light bg-primary-->
 
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Blog
-          </a>
-          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-            <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
-            <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
-            <a class="dropdown-item" href="blog-post.html">Blog Post</a>
-          </div>
-        </li>
+  <nav class="navbar fixed-top navbar-expand-lg navbar-primary bg-primary fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">GeziGen.TuR <i class="fas fa-umbrella-beach"></i></a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="services.php">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Blog
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+              <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
+              <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
+              <a class="dropdown-item" href="blog-post.html">Blog Post</a>
+            </div>
+          </li>
 
-      </ul>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
