767a0c7d03df81f05a33374957f257a23c66b535
AbdurrahimYILDIRIM <abdryldrm2018@gmail.com>
2020-04-08 18:20:53 +0300

    gereksiz sayfalar silindi navbar öğeleri düzenlendi

--- header.php
+++ header.php
@@ -24,34 +24,23 @@
 <body>
   <!--navbar  Konum ve içeriği   navbar-expand-lg navbar-light bg-primary-->
 
-  <nav class="navbar fixed-top navbar-expand-lg navbar-primary bg-primary fixed-top">
+  <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-primary fixed-top">
     <div class="container-fluid">
       <a class="navbar-brand" href="index.php">GeziGen.TuR <i class="fas fa-umbrella-beach"></i></a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon color-primary"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="about.php">About</a>
+            <a class="nav-link" href="about.php">Hakkımızda</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="services.php">Services</a>
+            <a class="nav-link" href="services.php">Hizmetlerimiz</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="contact.php">Contact</a>
+            <a class="nav-link" href="contact.php">İletişim</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Blog
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-              <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
-              <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
-              <a class="dropdown-item" href="blog-post.html">Blog Post</a>
-            </div>
-          </li>
-
         </ul>
       </div>
     </div>
